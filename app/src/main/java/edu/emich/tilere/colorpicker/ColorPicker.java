c5f18f2677201448fd4cb8e2259cf371c545fe02
Daltz333 <daltzsmith@gmail.com>
2023-11-02 20:59:08 -0400

    Successfully grabbed pointer coords and print to canvas

--- app/src/main/java/edu/emich/tilere/colorpicker/ColorPicker.java
+++ app/src/main/java/edu/emich/tilere/colorpicker/ColorPicker.java
@@ -7,11 +7,18 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 /***
  * Represents a color picker container.
@@ -21,9 +28,6 @@ import androidx.constraintlayout.widget.ConstraintLayout;
  * coordinates of their finger.
  */
 public class ColorPicker extends View {
-    // Container used by canvas to draw elements to the screen
-    private Paint m_drawPaint;
-
     // Canvas used for drawing components to the screen
     private Canvas m_canvas;
 
@@ -32,43 +36,98 @@ public class ColorPicker extends View {
 
     // Bitmap to be drawn
     private Bitmap m_bitmap;
+    private double m_lastXPos = 0.0;
+    private double m_lastYPos = 0.0;
+
+    private final TextPaint m_xPosPaint = new TextPaint();
+    private final TextPaint m_yPosPaint = new TextPaint();
 
     private final Rect m_bitmapContainer = new Rect();
 
+    private PriorityQueue<PaintItem> Paints = new PriorityQueue<>();
+
     public ColorPicker(Context context) {
         super (context);
-        
-        setupPaint();
+
+        init();
     }
     public ColorPicker(Context context, AttributeSet set) {
         super(context, set);
 
+        init();
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void init() {
         setupPaint();
+
+        this.setOnTouchListener(this::onTouch);
     }
 
     private void setupPaint() {
-        m_drawPaint = new Paint();
-        m_drawPaint.setColor(Color.BLACK);
-        m_drawPaint.setAntiAlias(true);
-        m_drawPaint.setStrokeWidth(5);
-
-        m_drawPaint.setStyle(Paint.Style.FILL_AND_STROKE);
-        m_drawPaint.setStrokeJoin(Paint.Join.ROUND);
-        m_drawPaint.setStrokeCap(Paint.Cap.ROUND);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         // Just in case, let's size check
         if (this.getWidth() != 0 || this.getHeight() != 0) {
-
             if (toDrawBitmap) {
                 processBitmap(canvas);
             }
+
+            m_xPosPaint.setAntiAlias(true);
+            m_xPosPaint.setTextSize(16 * getResources().getDisplayMetrics().density);
+            m_xPosPaint.setColor(0xFF000000);
+
+            m_yPosPaint.setAntiAlias(true);
+            m_yPosPaint.setTextSize(16 * getResources().getDisplayMetrics().density);
+            m_yPosPaint.setColor(0xFF000000);
+
+            String xPos = "X: " + m_lastXPos;
+            String yPos = "Y: " + m_lastYPos;
+
+            int xWidth = (int) m_xPosPaint.measureText(xPos);
+            int yWidth = (int) m_yPosPaint.measureText(yPos);
+
+            StaticLayout.Builder builder = StaticLayout.Builder.obtain(xPos, 0, xPos.length(), m_xPosPaint, xWidth);
+            StaticLayout xBuilder = builder.build();
+            xBuilder.draw(canvas);
+
+            canvas.translate(0, xBuilder.getHeight());
+
+            builder = StaticLayout.Builder.obtain(yPos, 0, yPos.length(), m_yPosPaint, yWidth);
+            StaticLayout yBuilder = builder.build();
+            yBuilder.draw(canvas);
+
         }
 
         this.m_canvas = canvas;
         super.onDraw(canvas);
+    }
+
+    /**
+     * Handles touch input to the canvas
+     * @param v
+     * @param event
+     * @return
+     */
+    public boolean onTouch(View v, MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        if (x < 0) {
+            x = 0;
+        }
+
+        if (y < 0) {
+            y = 0;
+        }
+
+        m_lastXPos = x;
+        m_lastYPos = y;
+
+        invalidate();
+        return true;
     }
 
     /**
