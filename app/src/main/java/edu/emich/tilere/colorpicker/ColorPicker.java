34b6ca2269cfd998682e96a35ec42a2940ec27b7
Daltz333 <daltzsmith@gmail.com>
2023-11-02 23:38:54 -0400

    Basic hex identification is functional, offsets are still wonky

--- app/src/main/java/edu/emich/tilere/colorpicker/ColorPicker.java
+++ app/src/main/java/edu/emich/tilere/colorpicker/ColorPicker.java
@@ -16,9 +16,13 @@ import android.view.View;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.Queue;
+
+import edu.emich.tilere.utils.AndroidUtils;
 
 /***
  * Represents a color picker container.
@@ -38,19 +42,24 @@ public class ColorPicker extends View {
 
     private final int m_crosshairWidth = 50;
     private final int m_crosshairLineThickness = 10;
+    private int m_selectedHexColor = 0;
 
     private final TextPaint m_xPosPaint = new TextPaint();
     private final TextPaint m_yPosPaint = new TextPaint();
-
+    private final TextPaint m_hexPaint = new TextPaint();
     private final Paint m_crossHairPaint = new Paint();
     private final Paint m_topLinePaint = new Paint();
     private final Paint m_leftLinePaint = new Paint();
     private final Paint m_rightLinePaint = new Paint();
     private final Paint m_bottomLinePaint = new Paint();
-
+    private final Paint m_previewPaint = new Paint();
     private final Rect m_bitmapContainer = new Rect();
 
-    private final Canvas m_canvasBuffer = new Canvas();
+    // Magic number because the y coordinate is offset from the top by
+    private final int kMagicOffset = 100;
+
+    // This is the bitmap that all objects are drawn upon
+    private Bitmap m_globalBitmap;
 
     public ColorPicker(Context context) {
         super (context);
@@ -65,12 +74,9 @@ public class ColorPicker extends View {
 
     @SuppressLint("ClickableViewAccessibility")
     private void init() {
-        setupPaint();
+        setDrawingCacheEnabled(true);
 
         this.setOnTouchListener(this::onTouch);
-    }
-
-    private void setupPaint() {
     }
 
     @Override
@@ -82,20 +88,32 @@ public class ColorPicker extends View {
                 processBitmap(canvas);
             }
 
+            int textColor = 0xFF000000;
+
+            if (AndroidUtils.isNightMode(getContext())) {
+                textColor = 0xFFFFFFFF;
+            }
+
             // DRAW DEBUG TEXT
             m_xPosPaint.setAntiAlias(true);
             m_xPosPaint.setTextSize(16 * getResources().getDisplayMetrics().density);
-            m_xPosPaint.setColor(0xFF000000);
+            m_xPosPaint.setColor(textColor);
 
             m_yPosPaint.setAntiAlias(true);
             m_yPosPaint.setTextSize(16 * getResources().getDisplayMetrics().density);
-            m_yPosPaint.setColor(0xFF000000);
+            m_yPosPaint.setColor(textColor);
+
+            m_hexPaint.setAntiAlias(true);
+            m_hexPaint.setTextSize(16 * getResources().getDisplayMetrics().density);
+            m_hexPaint.setColor(textColor);
 
             String xPos = "X: " + m_lastXPos;
             String yPos = "Y: " + m_lastYPos;
+            String hexColor = "Hex: #" + Integer.toHexString(m_selectedHexColor);
 
             int xWidth = (int) m_xPosPaint.measureText(xPos);
             int yWidth = (int) m_yPosPaint.measureText(yPos);
+            int hexWidth = (int) m_hexPaint.measureText(hexColor);
 
             StaticLayout.Builder builder = StaticLayout.Builder.obtain(xPos, 0, xPos.length(), m_xPosPaint, xWidth);
             StaticLayout xBuilder = builder.build();
@@ -106,6 +124,12 @@ public class ColorPicker extends View {
             builder = StaticLayout.Builder.obtain(yPos, 0, yPos.length(), m_yPosPaint, yWidth);
             StaticLayout yBuilder = builder.build();
             yBuilder.draw(canvas);
+
+            canvas.translate(0, yBuilder.getHeight());
+
+            builder = StaticLayout.Builder.obtain(hexColor, 0, hexColor.length(), m_hexPaint, hexWidth);
+            StaticLayout hexBuilder = builder.build();
+            hexBuilder.draw(canvas);
 
             // DRAW SQUARE CROSSHAIRS
             m_crossHairPaint.setColor(Color.RED);
@@ -118,12 +142,27 @@ public class ColorPicker extends View {
             m_topLinePaint.setStrokeWidth(m_crosshairLineThickness);
             canvas.drawLine((int)(m_lastXPos + (m_crosshairWidth / 2.0)), 0, (int)(m_lastXPos + (m_crosshairWidth / 2.0)), (int)m_lastYPos, m_topLinePaint);
 
-            // DRAW TOP LINE
+            // DRAW LEFT LINE
             m_leftLinePaint.setColor(Color.RED);
             m_leftLinePaint.setStrokeWidth(m_crosshairLineThickness);
             canvas.drawLine(0, (int)(m_lastYPos + (m_crosshairWidth / 2.0)), (int)m_lastXPos, (int)(m_lastYPos + (m_crosshairWidth / 2.0)), m_leftLinePaint);
 
+            // DRAW BOTTOM LINE
+            m_bottomLinePaint.setColor(Color.RED);
+            m_bottomLinePaint.setStrokeWidth(m_crosshairLineThickness);
+            canvas.drawLine((int)(m_lastXPos + (m_crosshairWidth / 2.0)), getHeight(), (int)(m_lastXPos + (m_crosshairWidth / 2.0)), (int)(m_lastYPos+m_crosshairWidth), m_topLinePaint);
 
+            // DRAW RIGHT LINE
+            m_rightLinePaint.setColor(Color.RED);
+            m_rightLinePaint.setStrokeWidth(m_crosshairLineThickness);
+            canvas.drawLine(getWidth(), (int)(m_lastYPos + (m_crosshairWidth / 2.0)), (int)(m_lastXPos+m_crosshairWidth), (int)(m_lastYPos + (m_crosshairWidth / 2.0)), m_leftLinePaint);
+
+            int previewX = getWidth() - (getWidth()/4);
+
+            try {
+                m_previewPaint.setColor(m_selectedHexColor);
+                canvas.drawRect(previewX, 0, previewX + 100, 100, m_previewPaint);
+            } catch (IllegalArgumentException ignored) {}
         }
 
         super.onDraw(canvas);
@@ -136,8 +175,8 @@ public class ColorPicker extends View {
      * @return
      */
     public boolean onTouch(View v, MotionEvent event) {
-        float x = event.getX();
-        float y = event.getY();
+        float x = getX() + event.getX();
+        float y = getY() + event.getY();
 
         if (x < 0) {
             x = 0;
@@ -153,6 +192,12 @@ public class ColorPicker extends View {
 
         m_lastXPos = x;
         m_lastYPos = y;
+
+        if (m_bitmap != null) {
+            if (x < m_bitmap.getWidth() && y < m_bitmap.getHeight()) {
+                m_selectedHexColor = m_bitmap.getPixel((int) x, (int) y);
+            }
+        }
 
         invalidate();
         return true;
