288d1893792bbab70ea565951ee1fef4039cc58d
Daltz333 <daltzsmith@gmail.com>
2023-11-24 12:10:43 -0500

    Add Endpoints

--- app/src/main/java/edu/emich/tilere/api/Endpoints.java
+++ app/src/main/java/edu/emich/tilere/api/Endpoints.java
@@ -1,4 +1,11 @@
 package edu.emich.tilere.api;
 
+/**
+ * Endpoints for API
+ * Used to build URIs
+ */
 public class Endpoints {
+    public static final String kDbHostUri = "http://grout.daltzsmith.com";
+    public static final String kSupportedApiVersion= "/api/1";
+    public static final String kAllGroutEndpoint = "/grout/";
 }
