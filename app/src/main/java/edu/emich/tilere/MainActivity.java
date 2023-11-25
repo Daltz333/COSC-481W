4f193af554e7cfecd68df8089468a9ce5d04835d
Daltz333 <daltzsmith@gmail.com>
2023-11-24 19:48:25 -0500

    Successfully retrieves grout from the remote db api

--- app/src/main/java/edu/emich/tilere/MainActivity.java
+++ app/src/main/java/edu/emich/tilere/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 
 import java.io.IOException;
 
+import edu.emich.tilere.api.DbClient;
 import edu.emich.tilere.colorpicker.ColorPicker;
 import edu.emich.tilere.utils.ImageUtils;
 
@@ -22,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Refresh grout
+        DbClient.getInstance().refreshGroutAsync();
     }
 
 }
