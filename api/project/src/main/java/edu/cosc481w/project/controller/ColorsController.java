efdfdc2962b9f3592c01f4c917c12c6fc53a2aea
me <sample.me>
2023-11-25 19:57:58 -0500

    Reset accidental changes
    # Conflicts:
    #	api/project/src/main/java/edu/cosc481w/project/service/ColorsService.java

--- api/project/src/main/java/edu/cosc481w/project/controller/ColorsController.java
+++ api/project/src/main/java/edu/cosc481w/project/controller/ColorsController.java
@@ -8,16 +8,27 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.cosc481w.project.dto.ColorsDto;
+import edu.cosc481w.project.service.ColorsService;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@RequestMapping("application/colors")
+@Slf4j
 public class ColorsController {
    
-   @GetMapping("/hello")
-   public String test() {
-      return "ok";
+   @Autowired
+   private ColorsService colorsService;
+
+   @GetMapping("/")
+   public ResponseEntity<List<ColorsDto>> getAll() {
+      try {
+         return new ResponseEntity<List<ColorsDto>>(colorsService.getAll(), HttpStatus.OK);
+      } catch (NoSuchElementException e) {
+         log.error("Unable to find " + e.getMessage());
+         return new ResponseEntity<List<ColorsDto>>(HttpStatus.NOT_FOUND);
+      }
    }
 }
