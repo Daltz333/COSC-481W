0656a07832fda8960b0b32310c0651614f7f55a2
Daltz333 <daltzsmith@gmail.com>
2023-11-27 18:53:47 -0500

    Adjusted themes and fonts

--- app/src/main/java/edu/emich/thp/MainActivity.java
+++ app/src/main/java/edu/emich/thp/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
 
 import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.appcompat.widget.Toolbar;
 
 import edu.emich.thp.api.DbClient;
 
@@ -15,8 +18,19 @@ public class MainActivity extends AppCompatActivity {
         SplashScreen.installSplashScreen(this);
         setContentView(R.layout.activity_main);
 
+        // Setup toolbar
+        Toolbar globalToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(globalToolbar);
+        globalToolbar.showOverflowMenu();
+
         // Refresh grout
         DbClient.getInstance().refreshGroutAsync();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return true;
+    }
 }
