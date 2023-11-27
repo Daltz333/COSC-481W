635d96bcc70c65bde730809f3b9464ba54ea27e2
Daltz333 <daltzsmith@gmail.com>
2023-11-27 18:15:29 -0500

    Add splash screen

--- app/src/main/java/edu/emich/thp/MainActivity.java
+++ app/src/main/java/edu/emich/thp/MainActivity.java
@@ -1,27 +1,18 @@
 package edu.emich.thp;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-
-import java.io.IOException;
 
 import edu.emich.thp.api.DbClient;
-import edu.emich.thp.colorpicker.ColorPicker;
-import edu.emich.thp.utils.ImageUtils;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SplashScreen.installSplashScreen(this);
         setContentView(R.layout.activity_main);
 
         // Refresh grout
